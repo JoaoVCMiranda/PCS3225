f8b629ccd1f4f0afc17b7602549276a435bae1af
João V C Miranda <joaovictorcmiranda@usp.br>
2024-08-14 17:29:54 -0300

    'Funcionou'

--- AF1/multiplicador_tb_arquivo.vhd
+++ AF1/multiplicador_tb_arquivo.vhd
@@ -9,10 +9,10 @@ library ieee;
 use ieee.numeric_bit.all;
 use std.textio.all;
 
-entity multiplicador_tb is
+entity multiplicador_tb_arquivo is
 end entity;
 
-architecture tb of multiplicador_tb is
+architecture tb of multiplicador_tb_arquivo is
   
   -- Componente a ser testado (Device Under Test -- DUT)
   component multiplicador
@@ -52,7 +52,7 @@ begin
                 Reset=>   rst_in,
                 Start=>   start_in,
                 Va=>      va_in,
-Vb=>      vb_in,
+		Vb=>      vb_in,
                 Vresult=> result_out,
                 Ready=>   ready_out
       );
@@ -60,21 +60,19 @@ Vb=>      vb_in,
   ---- PROCESS
   ---- Gera sinais de estimulo
   stimulus: process is
+  ---- Leitura do arquivo
+  file tb_file : text open read_mode is "multiplicador_tb.dat";
+	  variable tb_line: 		line;
+	  variable space: 		character;
+	  variable op1, op2 :		bit_vector(3 downto 0);
+	  variable produto_esperado :	bit_vector(7 downto 0);
   begin
 assert false report "Hora do pau" severity note;
   keep_simulating <= '1';
 
   rst_in <= '1'; start_in <= '0';
   wait for clockPeriod;
-
-
-  ---- Leitura do arquivo
-  file tb_file : text open read_mode is "multiplicador_tb.dat";
-	  variable tb_line: line;
-	  variable space: character;
-	  variable op1, op2, produto_esperado: bit_vector(3 downto 0);
-	     
-   begin
+  rst_in <= '0';
       while not endfile(tb_file) loop  -- Enquanto não chegar no final do arquivo ...
          readline(tb_file, tb_line);  -- Lê a próxima linha
          read(tb_line, op1);   -- Da linha que foi lida, lê o primeiro parâmetro (op1)
@@ -83,20 +81,22 @@ assert false report "Hora do pau" severity note;
          read(tb_line, space); -- Lê o próximo espaço usado como separador
          read(tb_line, produto_esperado);  -- Da linha que foi lida, lê o terceiro parâmetro (soma_esperada)
 		 -- Agora que já lemos o caso de teste (par estímulo/saída esperada), vamos aplicar os sinais.
-		 a_in <= op1;
-		 b_in <= op2;
+		va_in <= op1;
+		vb_in <= op2;
+		wait until falling_edge(clk_in);
+		start_in <= '1';
+		wait until falling_edge(clk_in);
+		start_in <= '0';
+		    -- espera pelo termino da multiplicacao
+		wait until ready_out='1';
+		    -- verifica resultado
 
-		 wait for clockPeriod; -- Aguarda a produção das saídas
+		wait for clockPeriod; -- Aguarda a produção das saídas
 		--  Verifica as saidas
          	assert result_out = produto_esperado report "Erro no Produto " & integer'image(to_integer(unsigned(op1))) & " * " & integer'image(to_integer(unsigned(op2))) severity error;
-
-		 
-		---- FALTA FAZER OS CASOS TESTE
-	 
-
-
 	end loop;
     assert false report "Teste concluido." severity note;	  
+    keep_simulating <= '0';
     wait; -- fim da simulação: aguarda indefinidamente
   end process;
 
