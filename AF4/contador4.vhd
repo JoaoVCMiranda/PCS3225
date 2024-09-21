4b5730873d4ebe1e16ea22d96ce1345474b5b7f6
João <joaovictorcmiranda@usp.br>
2024-09-21 01:13:22 -0300

    Modularizar

--- AF4/contador4.vhd
+++ AF4/contador4.vhd
@@ -1,2 +1,26 @@
 library IEEE;
 
+entity contador4 is
+	port (
+	    clock : in bit;
+	    zera  : in bit;
+	    en	  : in bit;
+	    Q     : out bit_vector(3 downto 0)
+	);
+end entity contador4;
+    
+architecture mycontador4 of contador4 is
+	    signal internal: bit_vector(3 downto 0);
+	    
+	    process(clock) is
+	    begin
+		if(clock'event and clock = '1') then
+		    if(en = '1')
+			internal = bit_vector ((unsigned (internal)) + "0001");
+		    end if;
+		    if(zera = '1') then internal <= "0000" end if;
+		end if;
+	    end process;
+	    Q <= internal;
+
+ end architecture; 
--- AF4/deslocador15.vhd
+++ AF4/deslocador15.vhd
@@ -4,25 +4,24 @@ use ieee.numeric_bit.all;
 entity deslocador15 is
     port (
         clock   : in bit;
-        limpa   : in bit;
-        carrega : in bit;
-        desloca : in bit;
+    	en	: in bit;
         entrada : in bit;
-        dados   : in bit_vector (14 downto 0);
         saida   : out bit_vector (14 downto 0)
     );    
 end entity deslocador15;
 
 architecture mydeslocador15 of deslocador15 is
-    process(clock) is
-    begin
-        if(clock'event and clock = '1') then
-            if(ED = '1') then
-                internal <= (inport);
-            elsif(Shift = '1') then
-                internal <= ('0' & internal(14 downto 1));
-            end if;
-        end if;
-    end process;
+	signal internal : bit_vector(14 downto 0);
+	process(clock) is
+		begin
+		if(clock'event and clock = '1') then
+			if(en = '1') then
+				internal <= '0' & entrada(14 downto 1);
+			else then
+				internal <= entrada;
+		    	end if;
+		end if;
+		saida <= internal;
+	end process;
 
 end architecture;
