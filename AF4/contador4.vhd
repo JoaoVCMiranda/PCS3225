c9fce944ae336ca2fc81141f253938d0917300fb
João <joaovictorcmiranda@usp.br>
2024-09-21 01:27:09 -0300

    Corrigindo erros

--- AF4/contador4.vhd
+++ AF4/contador4.vhd
@@ -3,7 +3,7 @@ library IEEE;
 entity contador4 is
 	port (
 	    clock : in bit;
-	    zera  : in bit;
+	    reset : in bit;
 	    en	  : in bit;
 	    Q     : out bit_vector(3 downto 0)
 	);
@@ -11,14 +11,15 @@ end entity contador4;
     
 architecture mycontador4 of contador4 is
 	    signal internal: bit_vector(3 downto 0);
-	    
+	    internal <= "0000";
 	    process(clock) is
 	    begin
 		if(clock'event and clock = '1') then
-		    if(en = '1')
+		    if(reset = '1') then 
+			internal <= "0000";
+		    elsif(en = '1') then
 			internal = bit_vector ((unsigned (internal)) + "0001");
 		    end if;
-		    if(zera = '1') then internal <= "0000" end if;
 		end if;
 	    end process;
 	    Q <= internal;
