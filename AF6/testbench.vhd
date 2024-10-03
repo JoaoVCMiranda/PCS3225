8f569c2dccfe0ca4a7ee8dcfd0c75e98d7636348
João <joaovictorcmiranda@>
2024-10-02 21:59:54 -0300

    Aleatoriedades

--- AF6/testbench.vhd
+++ AF6/testbench.vhd
@@ -7,7 +7,8 @@ end testbench;
 
 -- Completar com os sinais e componentes a serem utilizados
 architecture beh of testbench is
-	signal clk, run : bit;
+	signal clk : bit := '0'; 
+	signal run : bit;
 	component log is
 		port(
 			clock,inicio : in bit;
@@ -16,12 +17,24 @@ architecture beh of testbench is
 			fim : out bit
 		);
 	end component;
+	-- Signals
+	signal start 	: in bit;
+	signal EX 	: in bit_vector(7 downto 0 ) ;
+	signal AR 	: in bit_vector(7 downto 0 ) ;
+	signal ender 	: out bit
+
 	
 begin
 	-- Gerar o clock
 	clk <= ((not clk) and run) after 10 ns;
 	-- Instanciar o DUT
-	DUT:
+	DUT: log port map(
+			clock 	=> clk;
+			inicio 	=> start;
+			x 	=> EX;
+			R 	=> AR;
+			fim 	=> ender
+			);
 
 	-- Casos teste
 	process
