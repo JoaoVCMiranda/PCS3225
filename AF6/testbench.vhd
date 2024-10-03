063e7b2cb14101776525832c085e40cb16870b13
João <joaovictorcmiranda@>
2024-10-02 21:16:11 -0300

    Enunciado AF6

---
+++ AF6/testbench.vhd
@@ -0,0 +1,25 @@
+-- Completar com código do port do tb
+entity testbench is
+
+end testbench;
+
+-- Completar com os sinais e componentes a serem utilizados
+architecture beh of testbench is
+	signal clk, run : bit;
+	
+begin
+	-- Gerar o clock
+	clk <= ((not clk) and run) after 10 ns;
+	-- Instanciar o DUT
+	DUT:
+
+	-- Caso teste
+	process
+	begin 
+		run <= 1;
+		
+		run <= 0;
+		wait;
+	end process;
+end beh;
+
